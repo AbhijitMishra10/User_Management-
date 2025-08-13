18f58da24469c2c8356c1319cadc57e7a06ed0de
AbhijitMishra10 <abhijitmishra2000@gmail.com>
2025-08-13 10:04:25 +0530

    Delete Desktop/User_Management directory

--- Desktop/User_Management/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-// middleware/logger.js
-function logger(req,res,next) {
-    // Log the request method and URL
-    console.log(`${req.method} ${req.originalUrl} -> ${req.statusCode}`)
-    next() // Call the next middleware in the stack
-}
-// Export the logger middleware
-export default logger
